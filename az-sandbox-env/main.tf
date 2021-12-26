2e2c1c07424e86d3878ed35d77cf00ed2ff0f74e
hari om dubey <hariomdubey91@gmai.com>
2021-12-26 10:26:50 -0500

    change subs

--- az-sandbox-env/main.tf
+++ az-sandbox-env/main.tf
@@ -1,18 +1,18 @@
 #Create KeyVault ID
 resource "random_id" "kvname" {
   byte_length = 5
-  prefix = "keyvault"
+  prefix      = "keyvault"
 }
 
 #create the resource group
 resource "azurerm_resource_group" "rg2" {
-    name = "ateam-resource-group"
-    location = "australiaeast"
+  name     = "ateam-resource-group"
+  location = "australiaeast"
 }
 #Keyvault Creation
 data "azurerm_client_config" "current" {}
 resource "azurerm_key_vault" "kv1" {
-  depends_on = [ azurerm_resource_group.rg2 ]
+  depends_on                  = [azurerm_resource_group.rg2]
   name                        = random_id.kvname.hex
   location                    = "australiaeast"
   resource_group_name         = "ateam-resource-group"
@@ -20,7 +20,7 @@ resource "azurerm_key_vault" "kv1" {
   tenant_id                   = data.azurerm_client_config.current.tenant_id
   soft_delete_retention_days  = 7
   purge_protection_enabled    = false
-  sku_name = "standard"
+  sku_name                    = "standard"
   access_policy {
     tenant_id = data.azurerm_client_config.current.tenant_id
     object_id = data.azurerm_client_config.current.object_id
@@ -39,7 +39,7 @@ resource "azurerm_key_vault" "kv1" {
 
 #Create KeyVault VM password
 resource "random_password" "vmpassword" {
-  length = 20
+  length  = 20
   special = true
 }
 #Create Key Vault Secret
@@ -47,45 +47,45 @@ resource "azurerm_key_vault_secret" "vmpassword" {
   name         = "vmpassword"
   value        = random_password.vmpassword.result
   key_vault_id = azurerm_key_vault.kv1.id
-  depends_on = [ azurerm_key_vault.kv1 ]
+  depends_on   = [azurerm_key_vault.kv1]
 }
 
 
 #create the virtual network
 resource "azurerm_virtual_network" "vnet1" {
-    resource_group_name = azurerm_resource_group.rg2.name
-    location = "australiaeast"
-    name = "dev"
-    address_space = ["10.0.0.0/16"]
+  resource_group_name = azurerm_resource_group.rg2.name
+  location            = "australiaeast"
+  name                = "dev"
+  address_space       = ["10.0.0.0/16"]
 }
 
 #create a subnet within the virtual network
 resource "azurerm_subnet" "subnet1" {
-    resource_group_name = azurerm_resource_group.rg2.name
-    virtual_network_name = azurerm_virtual_network.vnet1.name
-    name = "devsubnet"
-    address_prefixes = ["10.0.0.0/24"]
+  resource_group_name  = azurerm_resource_group.rg2.name
+  virtual_network_name = azurerm_virtual_network.vnet1.name
+  name                 = "devsubnet"
+  address_prefixes     = ["10.0.0.0/24"]
 }
 
 ##create the network interface for the VM
 resource "azurerm_public_ip" "pub_ip" {
-    name = "vmpubip"
-    location = "australiaeast"
-    resource_group_name = azurerm_resource_group.rg2.name
-    allocation_method = "Dynamic"
+  name                = "vmpubip"
+  location            = "australiaeast"
+  resource_group_name = azurerm_resource_group.rg2.name
+  allocation_method   = "Dynamic"
 }
 
 resource "azurerm_network_interface" "vmnic" {
-    location = "australiaeast"
-    resource_group_name = azurerm_resource_group.rg2.name
-    name = "vmnic1"
+  location            = "australiaeast"
+  resource_group_name = azurerm_resource_group.rg2.name
+  name                = "vmnic1"
 
-    ip_configuration {
-        name = "vmnic1-ipconf"
-        subnet_id = azurerm_subnet.subnet1.id
-        private_ip_address_allocation = "Dynamic"
-        public_ip_address_id = azurerm_public_ip.pub_ip.id
-    }
+  ip_configuration {
+    name                          = "vmnic1-ipconf"
+    subnet_id                     = azurerm_subnet.subnet1.id
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.pub_ip.id
+  }
 }
 
 
@@ -96,14 +96,14 @@ resource "azurerm_network_interface" "vmnic" {
 #Create VM
 resource "azurerm_windows_virtual_machine" "dc01-vm" {
   name                = "dc01-vm"
-  depends_on = [ azurerm_key_vault.kv1 ]
+  depends_on          = [azurerm_key_vault.kv1]
   resource_group_name = azurerm_resource_group.rg2.name
   location            = "australiaeast"
   size                = "Standard_A1_v2"
   admin_username      = "hariom"
   admin_password      = azurerm_key_vault_secret.vmpassword.value
   network_interface_ids = [
-  azurerm_network_interface.vmnic.id,
+    azurerm_network_interface.vmnic.id,
   ]
   os_disk {
     caching              = "ReadWrite"
