cdf626c177c3cb44728bd563ed12398b32ed7fab
hari om dubey <hariomdubey91@gmai.com>
2021-12-30 09:26:39 -0500

    final

--- az-sandbox-env/main.tf
+++ az-sandbox-env/main.tf
@@ -62,7 +62,7 @@ resource "azurerm_virtual_network" "vnet1" {
     address_space = ["10.0.0.0/16"]
 }
 
-#create a subnet within the virtual network
+#create a subnet within the virtual networks
 resource "azurerm_subnet" "subnet1" {
     resource_group_name = azurerm_resource_group.rg2.name
     virtual_network_name = azurerm_virtual_network.vnet1.name
