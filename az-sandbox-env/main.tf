fc651b1ca3a702a38b6a7c4e587e9da3c9180b32
hari om dubey <hariomdubey91@gmai.com>
2021-12-30 06:03:53 -0500

    1

--- az-sandbox-env/main.tf
+++ az-sandbox-env/main.tf
@@ -53,7 +53,6 @@ resource "azurerm_key_vault_secret" "vmpassword" {
 
 
 
-
 #create the virtual network
 resource "azurerm_virtual_network" "vnet1" {
     resource_group_name = azurerm_resource_group.rg2.name
